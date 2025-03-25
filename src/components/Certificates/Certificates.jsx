8ba9fbdb16e1c2de91a5b4d0dad00ce61b510d53
Juin28 <tanjuinlong@gmail.com>
2025-03-25 21:46:00 +0100

    Add show more button at Certificates section

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
-import './Certificates.css'; 
+import { useState } from 'react';
 import { certificates } from '../../portfolio';
+import './Certificates.css';
 
 
 export default function Certificates() {
+  const [showAll, setShowAll] = useState(false);
+  const [buttonWord, setButtonWord] = useState("Show More")
+
   return (
     <section id="certificates" className='section certificates'>
       <h2 className='section__title'>Certificates</h2>
       <div className="certificates-list">
-        {certificates.map(certificate => (
-          <div className="certificate-card" key={`${certificate.title}-${certificate.organization}`}>
-            <h3 className="certificate-title">{certificate.title}</h3>
-            <p className="certificate-organization">{certificate.organization}</p>
-          </div>
-        ))}
+        {
+          showAll ? (
+            certificates.map(certificate => (
+              <div className="certificate-card" key={`${certificate.title}-${certificate.organization}`}>
+                <h3 className="certificate-title">{certificate.title}</h3>
+                <p className="certificate-organization">{certificate.organization}</p>
+              </div>
+            )
+            )) : (
+            certificates.slice(0, 4).map(certificate => (
+              <div className="certificate-card" key={`${certificate.title}-${certificate.organization}`}>
+                <h3 className="certificate-title">{certificate.title}</h3>
+                <p className="certificate-organization">{certificate.organization}</p>
+              </div>
+            ))
+          )
+        }
       </div>
+      <button
+        className='btn btn--outline projects__btn'
+        type='button'
+        onClick={() => {
+          setShowAll(!showAll)
+          setButtonWord(showAll ? "Show More" : "Show Less")
+        }}
+      >
+        {buttonWord}
+      </button>
     </section>
   );
 }
